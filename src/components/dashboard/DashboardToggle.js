ff19a0f5b7ff887e7092e245e028135c91d808a9
redhat2121 <redhat2121@protonmail.com>
2021-07-10 08:01:49 +0530

    added signin/signout functionality

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import { Button, Icon, Drawer } from 'rsuite';
-import { useModalState } from '../../misc/custom-hooks';
+import React, { useCallback } from 'react';
+import { Button, Icon, Drawer, Alert } from 'rsuite';
+import { useModalState, useMediaQuery } from '../../misc/custom-hooks';
 import Dashboard from '.';
+import { auth } from '../../misc/firebase';
 
 const DashboardToggle = () => {
   const { isOpen, close, open } = useModalState();
+  const isMobile = useMediaQuery('(max-width; 992px)');
+
+  const onSignOut = useCallback(() => {
+    auth.signOut();
+    Alert.info('signed out', 4000);
+    close();
+  }, [close]);
   return (
     <>
       <Button block clolor="blue" onClick={open}>
         <Icon icon="dashboard" /> Dashboard
       </Button>
-      <Drawer show={isOpen} outside={close} placement="left">
-        <Dashboard />
+      <Drawer full={isMobile} show={isOpen} outside={close} placement="left">
+        <Dashboard onSignOut={onSignOut} />
       </Drawer>
     </>
   );
