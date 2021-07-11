69118ecd1826e5153173ded5908f3fe5b6ac2675
redhat2121 <redhat2121@protonmail.com>
2021-07-11 17:49:37 +0530

    link and unlink fb and google accounts

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import { Button, Drawer } from 'rsuite';
+import { Alert, Button, Divider, Drawer } from 'rsuite';
 import { useProfile } from '../../context/Profile.context';
+import EditedableInput from '../EditedableInput';
+import { database } from '../../misc/firebase';
+import ProviderBlock from './ProviderBlock';
 
 const Dashboard = ({ onSignOut }) => {
   const { profile } = useProfile();
+
+  const onSave = async newData => {
+    const userNicknameRef = database
+      .ref(`/profiles/${profile.uid}`)
+      .child('name');
+
+    try {
+      await userNicknameRef.set(newData);
+      Alert.success('Nickname has been updated', 4000);
+    } catch (err) {
+      Alert.error(err.message, 4000);
+    }
+  };
   return (
     <>
       <Drawer.Header>
@@ -11,6 +27,14 @@ const Dashboard = ({ onSignOut }) => {
       </Drawer.Header>
       <Drawer.Body>
         <h3>Hey, {profile.name}</h3>
+        <ProviderBlock />
+        <Divider />
+        <EditedableInput
+          name="Nickname"
+          initialValue={profile.name}
+          onSave={onSave}
+          label={<h6 className="mb-2">Nickname</h6>}
+        />
       </Drawer.Body>
       <Drawer.Footer>
         <Button block color="red" onClick={onSignOut}>
