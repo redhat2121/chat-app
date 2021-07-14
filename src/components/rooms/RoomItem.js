a5ed5339447863dbf7821fd4ca6230e7746e45f7
redhat2121 <redhat2121@protonmail.com>
2021-07-14 08:27:46 +0530

    created chat rooms

--- src/components/rooms/RoomItem.js
+++ src/components/rooms/RoomItem.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import TimeAgo from 'timeago-react';
 
-const Roomitem = () => {
+const RoomItem = ({ room }) => {
+  const { createdAt, name } = room;
+
   return (
     <div>
       <div className="d-flex justify-content-between align-items-center">
-        <h3 className="text-disappear">Room name</h3>
-        <TimeAgo datetime={new Date()} className="font-normal text-black-45" />
+        <h3 className="text-disappear">{name}</h3>
+        <TimeAgo
+          datetime={new Date(createdAt)}
+          className="font-normal text-black-45"
+        />
       </div>
       <div className="d-flex align-items-center text-black-70">
         <span>No message yet</span>
@@ -15,4 +20,4 @@ const Roomitem = () => {
   );
 };
 
-export default Roomitem;
+export default RoomItem;
